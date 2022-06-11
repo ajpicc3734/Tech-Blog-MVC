f3c78a766eb9d9a308721ab5ba4ca753acc20097
ajpicc3734 <ajpicc3734@gmail.com>
2022-06-11 12:10:03 -0400

    update handlebars to populate homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,25 @@
 const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Blog, User, Comment } = require("../models");
 
 router.get("/", (req, res) => {
-  res.render("homepage");
+  Blog.findAll({
+    attributes: ["id", "body", "title", "created_at"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbBlogData) => {
+      const blogs = dbBlogData.map((blogs) => blogs.get({ plain: true }));
+      res.render("homepage", { blogs });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
